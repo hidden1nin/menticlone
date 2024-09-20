aef0312b6f19a9adb12051395030e835a1c7b273
hidden1nin <andrewletterman10@gmail.com>
2024-09-20 01:59:25 -0400

    change port

--- index.js
+++ index.js
@@ -168,7 +168,7 @@ wss.on('connection', (ws) => {
   });
 });
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 80;
 server.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
